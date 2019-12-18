6dd5bb30875fe706871c6381bea677b81e0ed824
mteiting <marcel.teiting@t-online.de>
2019-12-18 14:40:41 +0100

    init

---
+++ prototypes/item/item.lua
@@ -0,0 +1,19 @@
+data:extend(
+	{
+		{
+			type = "item",
+      name = "landfill",
+      icon = "__base__/graphics/icons/landfill.png",
+      icon_size = 32,
+      subgroup = "terrain",
+      order = "c[landfill]-a[dirt]",
+      stack_size = 500,
+      place_as_tile =
+      {
+        result = "landfill",
+        condition_size = 1,
+        condition = { "ground-tile" }
+      }
+		}
+  }
+)
